d39d75f6b2b8441a99d7881347cf3b09f9930692
Cherrystorm <lvreddyl@gmail.com>
2024-03-12 15:38:32 -0400

    Add CI Workflows]

---
+++ imports.tf
@@ -0,0 +1,48 @@
+# ##################################################################################
+# # IMPORTS
+# ##################################################################################
+
+# import {
+#   to = module.main.aws_vpc.this[0]
+#   id = "vpc-0742d361d0cbb719b" #VPC
+# }
+
+# import {
+#   to = module.main.aws_subnet.public[0]
+#   id = "subnet-00731173fd36028e5" #PublicSubnet1
+# }
+
+# import {
+#   to = module.main.aws_subnet.public[1]
+#   id = "subnet-0de6621628aa09c7f" #PublicSubnet2
+# }
+
+# import {
+#   to = module.main.aws_internet_gateway.this[0]
+#   id = "igw-00de78734ae4e856f" #InternetGateway
+# }
+
+# import {
+#   to = module.main.aws_route.public_internet_gateway[0]
+#   id = "rtb-05df44558be8f5a36_0.0.0.0/0" #DefaultPublicRoute
+# }
+
+# import {
+#   to = module.main.aws_route_table.public[0]
+#   id = "rtb-05df44558be8f5a36" #PublicRouteTable
+# }
+
+# import {
+#   to = module.main.aws_route_table_association.public[0]
+#   id = "subnet-00731173fd36028e5/rtb-05df44558be8f5a36" #PublicSubnet1/PublicRouteTable
+# }
+
+# import {
+#   to = module.main.aws_route_table_association.public[1]
+#   id = "subnet-0de6621628aa09c7f/rtb-05df44558be8f5a36" #PublicSubnet2/PublicRouteTable
+# }
+
+# import {
+#   to = aws_security_group.ingress
+#   id = "sg-08592b4dde9a79389" #NoIngressSecurityGroup
+# }
